635945b9f1e5f41008f69f789f53d6f789f5abdc
Alonso Vega <lavb91@gmail.com>
2013-03-12 17:25:40 -0600

    First commit

---
+++ Sample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Sample
+//
+//  Created by Móviles on 12/03/13.
+//  Copyright (c) 2013 Comunidad de Dispositivos Móviles. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Sample/MyTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  MyTableViewController.h
+//  Sample
+//
+//  Created by Móviles on 12/03/13.
+//  Copyright (c) 2013 Comunidad de Dispositivos Móviles. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyTableViewController : UITableViewController
+
+@end
